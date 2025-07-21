22ebfb7c014187ae6e9c37de5469eb7bb75f89ab
jishnay nair <jish261203@gmail.com>
2025-07-21 11:03:15 +0530

    Welcome message

--- E-commerce backend/app/main.py
+++ E-commerce backend/app/main.py
@@ -3,5 +3,9 @@ from app.routes import products,orders
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "HROne Backend API is running ✅"}
+
 app.include_router(products.router)
 app.include_router(orders.router)
